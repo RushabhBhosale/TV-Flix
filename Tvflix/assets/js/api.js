0115ea07331738a06fce9fb6108f7251b658a53d
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2023-10-06 12:27:22 +0530

    Movie Website Complete

--- Tvflix/assets/js/api.js
+++ Tvflix/assets/js/api.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const api_key = 'b908b69cbf1920367b43cf85750d64db';
-const imageBaseURL = 'http://image.tmdb.org/t/p/';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
 
-// Fetch data
+
 
 const fetchDataFromServer = function (url, callback, optionalParam) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => callback(data, optionalParam));
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
 }
 
 export { imageBaseURL, api_key, fetchDataFromServer };
